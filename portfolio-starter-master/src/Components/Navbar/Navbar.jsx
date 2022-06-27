181dc59efbea643353870948818073f76514dc29
Lívia Martins <lilivitt001@gmail.com>
2022-06-27 20:50:43 -0300

    Navbar added

---
+++ portfolio-starter-master/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+        <div className="n-wrapper">
+            <div className="n-left">
+                <div className="n-name">Lívia</div>
+                <span>toggle</span>
+            </div>
+            <div className="n-right">
+                <div className="n-list">
+                    <ul style={{listStyleType: 'none'}}>
+                        <li>Home</li>
+                        <li>Services</li>
+                        <li>Experience</li>
+                        <li>Portfolio</li>
+                        <li>Testimonials</li>
+                    </ul>
+                </div>
+                <button className="button n-button">
+                    Contact Me
+                </button>
+            </div>
+        </div>
+    );
+};
+ export default Navbar
